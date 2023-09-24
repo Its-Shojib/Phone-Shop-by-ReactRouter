22ae447fa5f6c2ed8044d3ec5d411b375c35d1b7
Its_Shojib <mdshojib922@gmail.com>
2023-09-24 09:42:29 +0600

    FAvorite added

--- src/Pages/Favorite/Favorite.jsx
+++ src/Pages/Favorite/Favorite.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
 
 
 const Favorite = () => {
+    let [favorites,setFavorites] = useState([]);
+    let [noFound,setNoFound] = useState('');
+
+    useEffect(()=>{
+       let favoriteItem = JSON.parse(localStorage.getItem('favorites'));
+       if(favoriteItem){
+           setFavorites(favoriteItem);
+       }
+       else{
+        setNoFound('No data found')
+       }
+    },[])
+    console.log(favorites);
     return (
         <div className="min-h-screen">
-            <p>This is Favorite</p>
+            {
+                noFound ? <p>{noFound}</p> : <div>
+                    
+                </div>
+            }
         </div>
     );
 };
